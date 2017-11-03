e315d49c84ef426d4fe1693e3cd22be445b9bc54
singingsea <33134727+singingsea@users.noreply.github.com>
2017-11-03 17:57:49 -0400

    fix a bug

--- assign_refspec_time_to_avgvcd.m
+++ assign_refspec_time_to_avgvcd.m
@@ -1,46 +1,46 @@
 function [avg_vcd,rcd_S] = assign_refspec_time_to_avgvcd(dscd_S,avg_vcd,rcd_S)
 %% assign ref spec time to avg_vcd
 N = size(avg_vcd.day);
+disp('Assign reference spec time to VCD structure ...');
 for i = 1:1:N(1)
     TF = dscd_S.day == avg_vcd.day(i);
     try 
-        print('Assign reference spec time to VCD structure.');
         if sum(TF) > 0 
             ref_sza = dscd_S.ref_sza(TF,:);
             ref_sza_utc1 = dscd_S.ref_sza_utc1(TF,:);
             ref_sza_utc2 = dscd_S.ref_sza_utc2(TF,:);
         else
-            print('Warning: a day number in avg_vcd is not found in dscd_S! Pls check these two structure!');
+            disp('Warning: a day number in avg_vcd is not found in dscd_S! Pls check these two structure!');
         end
 
-        avg_vcd.ref_sza(i) = ref_sza(1);
-        avg_vcd.ref_sza_utc1(i) = ref_sza_utc1(1);
-        avg_vcd.ref_sza_utc2(i) = ref_sza_utc2(1);
+        avg_vcd.ref_sza(i,:) = ref_sza(1);
+        avg_vcd.ref_sza_utc1(i,:) = ref_sza_utc1(1);
+        avg_vcd.ref_sza_utc2(i,:) = ref_sza_utc2(1);
     catch
-        print('Warning: Failled to assign reference spec time to VCD structure!');
-        print('Warning: No reference spec time will be stored to the final VCD files!');
+        disp('Warning: Failled to assign reference spec time to VCD structure!');
+        disp('Warning: No reference spec time will be stored to the final VCD files!');
     end
         
 end
 %% assign ref spec time to rcd_S
 N = size(rcd_S.day);
+disp('Assign reference spec time to RCD structure ...');
 for i = 1:1:N(1)
     TF = dscd_S.day == rcd_S.day(i);
     try 
-        print('Assign reference spec time to RCD structure.');
         if sum(TF) > 0 
             ref_sza = dscd_S.ref_sza(TF,:);
             ref_sza_utc1 = dscd_S.ref_sza_utc1(TF,:);
             ref_sza_utc2 = dscd_S.ref_sza_utc2(TF,:);
         else
-            print('Warning: a day number in rcd_S is not found in dscd_S! Pls check these two structure!');
+            disp('Warning: a day number in rcd_S is not found in dscd_S! Pls check these two structure!');
         end
 
-        rcd_S.ref_sza(i) = ref_sza(1);
-        rcd_S.ref_sza_utc1(i) = ref_sza_utc1(1);
-        rcd_S.ref_sza_utc2(i) = ref_sza_utc2(1);
+        rcd_S.ref_sza(i,:) = ref_sza(1);
+        rcd_S.ref_sza_utc1(i,:) = ref_sza_utc1(1);
+        rcd_S.ref_sza_utc2(i,:) = ref_sza_utc2(1);
     catch
-        print('Warning: Failled to assign reference spec time to RCD structure!');
+        disp('Warning: Failled to assign reference spec time to RCD structure!');
     end
         
 end
