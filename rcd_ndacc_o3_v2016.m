b5cdc44563eb789c0d62ce4adbae77f97b629be1
KristofBognar <kbognar@physics.utoronto.ca>
2017-11-08 12:13:27 -0800

    Restored code to change code_path in rcd_ndacc_o3. Code depends on where the AMF_LUTs are located

--- rcd_ndacc_o3_v2016.m
+++ rcd_ndacc_o3_v2016.m
@@ -51,12 +51,14 @@ function [rcd_vec, amf] = ...
 %       amf : AMF corresponding with each index
 
 % put LUT folder pareller to VCD code folder!
-% working_dir = pwd; % record current working dir
-% mkdir('AMF');% make folder to save all AMF outputs from LUT
-% cd(code_path);
-% cd ..
-% code_path = [pwd];% this should be the path for LUT folder!
-% cd(working_dir);% go back to original working dir
+if ispc
+    working_dir = pwd; % record current working dir
+    mkdir('AMF');% make folder to save all AMF outputs from LUT
+    cd(code_path);
+    cd ..
+    code_path = [pwd];% this should be the path for LUT folder!
+    cd(working_dir);% go back to original working dir
+end
 
 % / works for paths on Windows as well
 if O3_AMF_version == 1
